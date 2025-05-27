b58472142930592c9336cbd37f2416d510fe5440
Simon Walker <github@stwalkerster.co.uk>
2025-05-27 13:59:46 +0000

    Add codespaces setup script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Sets up GitHub Codespaces
+
+rm -f ~/.nanorc ~/.screenrc ~/.gitconfig ~/.bashrc
+
+sed -r -i gitconfig -e 's/^( +)signingkey/\1# signingkey/'
+
+ln -s ${PWD}/nanorc ~/.nanorc
+ln -s ${PWD}/screenrc ~/.screenrc
+ln -s ${PWD}/gitconfig ~/.gitconfig
+ln -s ${PWD}/bashrc ~/.bashrc
+
